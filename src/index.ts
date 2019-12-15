225443a37d784c8d17e7541924fdfc29bf6ac307
viethoang92 <vi_hoang@hotmail.de>
2019-12-15 16:42:52 +0100

    Init dev branch

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ let bot = container.get<Bot>(TYPES.Bot);
 bot.listen().then(() => {
     console.log('Logged in!')
 }).catch((error) => {
-    console.log('Oh no! ', error)
+    console.log('Oh no, Error! ', error)
 });
