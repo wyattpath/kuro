840903c6ff5f9022413593dc61f8d5b944932e3a
viethoang92 <vi_hoang@hotmail.de>
2019-12-15 16:01:25 +0100

    Init

---
+++ tests/src/integration.spec.ts
@@ -0,0 +1,22 @@
+import 'mocha';
+import container from "../../inversify.config";
+import {TYPES} from "../../src/types";
+import {Client} from "discord.js";
+import {Bot} from "../../src/bot";
+import {instance, mock} from "ts-mockito";
+describe('Bot', () => {
+    let discordMock: Client;
+    let discordInstance: Client;
+    let bot: Bot;
+
+    beforeEach(() => {
+        discordMock = mock(Client);
+        discordInstance = instance(discordMock);
+        container.rebind<Client>(TYPES.Client)
+            .toConstantValue(discordInstance);
+        bot = container.get<Bot>(TYPES.Bot);
+    });
+
+    // Test cases here
+
+});
